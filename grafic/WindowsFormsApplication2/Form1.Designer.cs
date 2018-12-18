fdbc7a7bce914362402661d4ef76e116fafa1b75
danixweb <corosd@yahoo.com>
2018-12-18 22:34:52 +0200

    Add files via upload

---
+++ grafic/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Grafic
+{
+    partial class frmGrafic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(492, 9);
+            this.trackBar1.Maximum = 400;
+            this.trackBar1.Minimum = 200;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(158, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 300;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(396, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marime axe";
+            // 
+            // frmGrafic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(695, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGrafic";
+            this.Text = "Grafic";
+            this.Load += new System.EventHandler(this.frmGrafic_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+
+
+    }
+}
+
---
+++ grafic/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Grafic
+{
+    public partial class frmGrafic : Form
+    {
+        int[] LungimeBare=new int [10];
+        int PozitieDreapta=100;
+        int PozitieJos=400;
+        int InaltimeMaximaBara = 100;
+        int Aliniere = 30;
+        int LatimeBara = 25;
+        int LungimeAxaX=300;
+        int LungimeAxaY = 300;
+        int MarimeSageata = 4;
+        int MultiplicareBara = 1;
+        Point inceput = new Point();
+        Point sfarsit = new Point();
+        System.Drawing.Graphics Desen;
+        System.Drawing.Pen Creion_negru;
+        System.Drawing.Pen Creion_albastru;
+        Random random = new Random();
+        
+      
+        public frmGrafic()
+        {
+            InitializeComponent();
+        }
+        public void NumereAleatorii()
+        {
+            int i;
+            for (i = 0; i <= 9;i++)
+            {
+                LungimeBare[i]= random.Next(0, 100);
+                inceput.X = PozitieDreapta + i * Aliniere;
+                inceput.Y = PozitieJos;
+                sfarsit.X = PozitieDreapta + i * Aliniere;
+                sfarsit.Y = PozitieJos-InaltimeMaximaBara - LungimeBare[i];
+                Desen.DrawRectangle(Creion_albastru, inceput.X + 5, PozitieJos - InaltimeMaximaBara + LungimeBare[i], LatimeBara, (InaltimeMaximaBara - LungimeBare[i]) );
+
+
+                Point LocText = new Point();
+
+                Font font = new Font("Arial", Convert.ToSingle(trackBar1.Value) / 20);
+                LocText.X = PozitieDreapta + 10 + i * Aliniere;
+                LocText.Y = 280 + LungimeBare[i];
+                string valoare = Convert.ToString(InaltimeMaximaBara - LungimeBare[i]);
+                Desen.DrawString(valoare, font, Brushes.Green, LocText);
+                LocText.X = Aliniere+PozitieDreapta + i * Aliniere;
+                LocText.Y = PozitieJos;
+               valoare = Convert.ToString(i);
+               TextInclinat(Desen, 90, "Val" + valoare, LocText.X, LocText.Y, font, Brushes.Red);
+               
+            }
+        }
+         private void TextInclinat(Graphics gr, float angle,
+            string txt, int x, int y, Font the_font, Brush the_brush)
+        {
+            // Salvare stare zona de desenare.
+            GraphicsState state = gr.Save();
+            gr.ResetTransform();
+            // Rotim zona de desen.
+            gr.RotateTransform(angle);
+            // Deplasam textul la pozitia dorita.
+            gr.TranslateTransform(x, y, MatrixOrder.Append);
+             // desenam textul in origine.
+            gr.DrawString(txt, the_font, the_brush, 0, 0);
+            // Restabilim starea initiala a zonei de desen.
+            gr.Restore(state);
+        }
+        private void TraseazaAxe()
+        {
+            Desen = this.CreateGraphics();
+            Desen.Clear(this.BackColor);
+            Creion_negru = new System.Drawing.Pen(System.Drawing.Color.Black);
+            Creion_negru.Width = 2;
+            Creion_albastru = new System.Drawing.Pen(System.Drawing.Color.Blue);
+            inceput.X = PozitieDreapta;
+            inceput.Y = PozitieJos;
+            sfarsit.X = PozitieDreapta + LungimeAxaX;
+            sfarsit.Y = PozitieJos;
+            Desen.DrawLine(Creion_negru, inceput, sfarsit);
+            Desen.DrawLine(Creion_negru, PozitieDreapta, PozitieJos - LungimeAxaY, PozitieDreapta + MarimeSageata, PozitieJos - LungimeAxaY + MarimeSageata);
+            Desen.DrawLine(Creion_negru, PozitieDreapta, PozitieJos - LungimeAxaY, PozitieDreapta - MarimeSageata, PozitieJos - LungimeAxaY + MarimeSageata);
+            sfarsit.X = PozitieDreapta;
+            sfarsit.Y = PozitieJos - LungimeAxaY;
+            Desen.DrawLine(Creion_negru, inceput, sfarsit);
+            Desen.DrawLine(Creion_negru, PozitieDreapta + LungimeAxaX + MarimeSageata, PozitieJos, PozitieDreapta + LungimeAxaX, PozitieJos - MarimeSageata);
+            Desen.DrawLine(Creion_negru, PozitieDreapta + LungimeAxaX + MarimeSageata, PozitieJos, PozitieDreapta + LungimeAxaX, PozitieJos + MarimeSageata);
+ }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TraseazaAxe();
+            NumereAleatorii();
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            LungimeAxaX = Convert.ToInt32(trackBar1.Value);
+            LungimeAxaY = Convert.ToInt32(trackBar1.Value);
+            MarimeSageata = Convert.ToInt32(trackBar1.Value) / 80;
+            Creion_negru.Width = Convert.ToInt32(trackBar1.Value) / 120;
+            LatimeBara = Convert.ToInt32(trackBar1.Value) / 15;
+            Aliniere = Convert.ToInt32(trackBar1.Value) / 10;
+            MultiplicareBara=Convert.ToInt32(trackBar1.Value) / 100;
+        }
+
+        private void frmGrafic_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+
